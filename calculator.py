7ad1b6cd2213a98609a8c4b5290b0a10c58fc981
saloni23j <mathursaloni123@gmail.com>
2024-04-28 22:21:53 +0530

    initial commit

---
+++ calculator.py
@@ -0,0 +1,11 @@
+result = add(5, 3)
+print("5 + 3 =", result)
+
+result = subtract(7, 2)
+print("7 - 2 =", result)
+
+result = multiply(4, 6)
+print("4 * 6 =", result)
+
+result = divide(8, 2)
+print("8 / 2 =", result)
